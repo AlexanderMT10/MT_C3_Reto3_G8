4b2e6eb031fbcc609af1e2e0e0aaa67ebdb2cdc9
John Peña <john.pena.mt@correo.usa.edu.co>
2021-10-07 19:02:17 -0500

    Creacion de controller de client

---
+++ springBoogtG8/src/main/java/com/game/g8/sa/reto3grupo8/controller/ClientController.java
@@ -0,0 +1,49 @@
+package com.game.g8.sa.reto3grupo8.controller;
+
+import com.game.g8.sa.reto3grupo8.entity.Client;
+import com.game.g8.sa.reto3grupo8.service.ClientServicio;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author JhonA
+ */
+@RestController
+@RequestMapping("/api/client/")
+public class ClientController {
+    
+    @Autowired
+    private ClientServicio service;
+    
+    @GetMapping("/all")
+    public List<Client> findAllClient(){
+        return service.getClientAll();
+    }
+    
+    @PostMapping("/save")
+    public ResponseEntity addClient(@RequestBody Client client){
+        service.saveClient(client);
+        return ResponseEntity.status(201).build();
+    }
+    
+    @PutMapping("/update")
+    public ResponseEntity updateClient(@RequestBody Client client){
+        service.updateClient(client);
+        return ResponseEntity.status(201).build();
+    }
+    
+    @DeleteMapping("/dalete")
+    public ResponseEntity deleteClient(@RequestBody Client client){
+        service.deleteClient(client.getId());
+        return ResponseEntity.status(201).build();
+    }
+}
